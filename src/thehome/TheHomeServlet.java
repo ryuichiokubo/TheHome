2b9680a7ef69043df6bb36654e4c7e65de04a92c
Ryuichi Okubo <ryuichi.okubo@blaast.com>
2013-01-29 17:35:51 +0200

    rss-fetch: parse contents by rome

--- src/thehome/TheHomeServlet.java
+++ src/thehome/TheHomeServlet.java
@@ -7,6 +7,12 @@ import java.net.URL;
 
 import javax.servlet.http.*;
 
+import com.sun.syndication.feed.synd.SyndEntry;
+import com.sun.syndication.feed.synd.SyndFeed;
+import com.sun.syndication.io.FeedException;
+import com.sun.syndication.io.SyndFeedInput;
+import com.sun.syndication.io.XmlReader;
+
 @SuppressWarnings("serial")
 public class TheHomeServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
@@ -16,17 +22,38 @@ public class TheHomeServlet extends HttpServlet {
 		
 		// fetch data from URL
 		URL url = new URL("http://rss.dailynews.yahoo.co.jp/fc/rss.xml");
+		SyndFeedInput input = new SyndFeedInput();
+		SyndFeed feed = null;
+		try {
+			feed = input.build(new XmlReader(url));
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (FeedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		// parse RSS
+		String title = feed.getTitle();
+		String res = "";
+		for (Object obj : feed.getEntries()) {
+			SyndEntry entry = (SyndEntry) obj;
+			res += entry.getTitle();
+			res += "\n";
+		}
+		/*
 		BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"));
 		String line = "";
-		String res = "";
 		while ((line = reader.readLine()) != null) {
 			res += line;
 		}
+		*/
 		
 		// response
 		resp.setContentType("text/plain");
 		resp.setCharacterEncoding("UTF-8");
-		resp.getWriter().println("Yahoo!トピックス");
+		resp.getWriter().println(title);
 		resp.getWriter().println(res);
 	}
 }
